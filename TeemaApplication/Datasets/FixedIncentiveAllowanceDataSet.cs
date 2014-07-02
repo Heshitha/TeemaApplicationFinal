90db35b470bab01d3c21926dc3db308ee6758ba0
prabhathanuradha <stpraba@gmail.com>
2014-07-02 01:15:29 -0700

    SalaryAdvance

---
+++ TeemaApplication/Datasets/FixedIncentiveAllowanceDataSet.cs
@@ -0,0 +1,12 @@
+﻿namespace TeemaApplication.Datasets {
+    
+    
+    public partial class FixedIncentiveAllowanceDataSet {
+    }
+}
+namespace TeemaApplication.Datasets {
+    
+    
+    public partial class FixedIncentiveAllowanceDataSet {
+    }
+}
